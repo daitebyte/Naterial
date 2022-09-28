5b2021cb6f7b247d4bb61f37578c74136be0543e
daitebyte <daitebyte@gmail.com>
2022-09-28 23:26:05 +0300

    feat: add default corner radius & assist

---
+++ src/Naterial/Controls/CornerRadiusAssist.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+
+namespace Naterial.Controls
+{
+    /// <summary>
+    /// Provides attached properties for setting the corner radius of an element.
+    /// </summary>
+    public static class CornerRadiusAssist
+    {
+        #region Attached Property: CornerRadius
+        /// <summary>
+        /// Identifies the <see cref="CornerRadiusAssist" />.CornerRadius attached property.
+        /// </summary>
+        public static readonly DependencyProperty CornerRadiusProperty = DependencyProperty.RegisterAttached(
+            "CornerRadius",
+            typeof(CornerRadius),
+            typeof(CornerRadiusAssist),
+            new FrameworkPropertyMetadata(
+                new CornerRadius(0),
+                FrameworkPropertyMetadataOptions.AffectsRender | FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+        /// <summary>
+        /// Gets the value of the <see cref="CornerRadiusAssist" />.CornerRadius attached property from a given <see cref="UIElement" />.
+        /// </summary>
+        /// <param name="element">
+        /// The element from which to read the property value.
+        /// </param>
+        /// <returns>
+        /// The value of the <see cref="CornerRadiusAssist" />.CornerRadius attached property.
+        /// </returns>
+        public static CornerRadius GetCornerRadius(UIElement element) => (CornerRadius)element.GetValue(CornerRadiusProperty);
+
+        /// <summary>
+        /// Sets the value of the <see cref="CornerRadiusAssist" />.CornerRadius attached property to a given <see cref="UIElement" />.
+        /// </summary>
+        /// <param name="element">
+        /// The element on which to set the <see cref="CornerRadiusAssist" />.CornerRadius attached property.
+        /// </param>
+        /// <param name="value">
+        /// The property value to set.
+        /// </param>
+        public static void SetCornerRadius(UIElement element, CornerRadius value) => element.SetValue(CornerRadiusProperty, value);
+        #endregion
+    }
+}
